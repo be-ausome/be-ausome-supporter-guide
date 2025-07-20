b8438c94d0fb576eb498e291a082abe644e7017c
be-ausome <dave@be-ausome.com>
2025-07-20 10:58:38 -0600

    Delete api directory

--- api/hello.js
+++
@@ -1,8 +0,0 @@
-// /api/hello.js
-export default function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    message: "Supporter GPT API is alive!",
-    timestamp: Date.now()
-  });
-}
