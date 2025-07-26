98dcc4c2bdaa87eb34c16f3d834008262abefdc0
David Miller <dave@be-ausome.com>
2025-07-26 09:04:19 -0600

    Remove deprecated experimental.appDir flag

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-﻿import type { NextConfig } from "next";
+﻿import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true },
   trailingSlash: false
 };
 
 export default nextConfig;
+
